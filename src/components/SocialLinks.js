b566f0e1b1287240f11c99687cfb298d77d0631f
Kelly Williams <105178236+kellyfayrishta@users.noreply.github.com>
2023-02-20 20:42:42 -0500

    added sociallinks.js

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 export const SocialLinks = () => {
   return (
-    <div>SocialLinks</div>
-  )
-}
+    <div>
+      <ul>
+        <li>
+          <a href="">
+            {" "}
+            <>
+              Linkedin <FaLinkedin />
+            </>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default SocialLinks;
