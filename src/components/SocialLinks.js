50254af779348c2995a454c536151d231c4d5e28
Kelly Williams <105178236+kellyfayrishta@users.noreply.github.com>
2023-02-25 17:20:11 -0500

    added social links

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -4,17 +4,72 @@ import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 
 export const SocialLinks = () => {
+
+    const links = [
+      {
+        id: 1,
+        child: (
+          <>
+            Linkedin <FaLinkedin size={30} />
+          </>
+        ),
+        href: "https://linkedin.com/in/kelly-williams-54516a6a/",
+        style: "rounded-tr-md",
+      },
+      {
+        id: 2,
+        child: (
+          <>
+            GitHub <FaGithub size={30} />
+          </>
+        ),
+        href: "https://github.com/kellyfayrishta",
+      },
+      {
+        id: 3,
+        child: (
+          <>
+            Mail <HiOutlineMail size={30} />
+          </>
+        ),
+        href: "mailto:kellyfayrishtadev@gmail.com",
+      },
+      {
+        id: 4,
+        child: (
+          <>
+            Resume <BsFillPersonLinesFill size={30} />
+          </>
+        ),
+        href: "/Kelly_Williams_Resume.pdf",
+        style: "rounded-br-md",
+        download: true,
+      },
+    ];
+
   return (
-    <div>
+    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
       <ul>
-        <li>
-          <a href="">
-            {" "}
-            <>
-              Linkedin <FaLinkedin />
-            </>
-          </a>
-        </li>
+        {links.map(({ id, child, href, style, download }) => (
+          <li
+            key={id}
+            className={
+              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
+              " " +
+              style
+            }
+          >
+            <a
+              href={href}
+              className="flex justify-between items-center w-full text-white"
+              download={download}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {child}
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   );
