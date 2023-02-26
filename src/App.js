85d5a414887b3579032c4fcc19d867202a54a62d
Kelly Williams <105178236+kellyfayrishta@users.noreply.github.com>
2023-02-25 19:53:30 -0500

    added and styled contact.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import  Home  from "./components/Home";
 import  Navbar  from "./components/Navbar";
 import  Portfolio   from "./components/Portfolio";
 import  SocialLinks  from "./components/SocialLinks";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <About />
       <Portfolio />
       <Experience />
+      <Contact />
 
       <SocialLinks />
     </div>
